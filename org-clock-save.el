8f67dfaca1962839549b7ebf1ae3ed9ad136c833
Siddharth <sidharth3930@gmail.com>
2018-09-10 11:28:03 -0500

    use dropbox for org-mode

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,1 +1,2 @@
-;; org-clock-save.el - Siddharths-MacBook-Pro.local at <2018-03-02 Fri 02:22>
+;; org-clock-save.el - main-10-231-100-95.tamulink.tamu.edu at <2018-09-10 Mon 08:46>
+(setq org-clock-stored-history '(("/Users/subsid/Dropbox/notes/work.org" . 48148)))
