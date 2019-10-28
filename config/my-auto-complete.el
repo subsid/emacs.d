889501b231738526b82121280fa3854a7a7aa466
Siddharth <ssubramaniyam@etsy.com>
2019-10-28 09:37:59 -0400

    Ledger mode hook for autocomplete

--- config/my-auto-complete.el
+++ config/my-auto-complete.el
@@ -7,6 +7,12 @@
     (define-key company-active-map (kbd "C-n") 'company-select-next)
     (global-set-key (kbd "C-.") 'company-complete-common-or-cycle)))
 
+(add-hook 'ledger-mode-hook
+	  (lambda ()
+	    (setq-local tab-always-indent 'complete)
+	    (setq-local completion-ignore-case t)))
+
+
 (use-package ycmd
   :ensure t
   :config
